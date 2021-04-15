13fb1fdbfab822f972e6df69abf0abb6f44fa078
luatptbeta <luatphamdhv@gmail.com>
2021-04-15 22:03:30 +0700

    Update login

--- src/common/Footer.js
+++ src/common/Footer.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import LoginForm from '../member/component/login/LoginForm'
 
 class Footer extends React.PureComponent{
 
     render(){
         return(
             <div>
+            <LoginForm />
             <p>Pham Tien Luat</p>
             </div>
         )
